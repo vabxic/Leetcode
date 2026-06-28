789fce1254f0e1bd660bace076f32a7fab2fe3df
Vaibhav Singh <vaibhavsingh4805@gmail.com>
2026-06-28 12:49:57 +0530

    Time: 1070 ms (89.36%), Space: 515.4 MB (33.02%) - LeetHub

--- 3527-find-the-most-common-response/3527-find-the-most-common-response.cpp
+++ 3527-find-the-most-common-response/3527-find-the-most-common-response.cpp
@@ -1,3 +1,5 @@
+
+/*approach 1 1900ms;
 auto init = []() {
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -28,5 +30,34 @@ public:
         });
 
         return vec[0].first;
+    }
+};
+*/
+class Solution {
+public:
+    string findCommonResponse(vector<vector<string>>& res) {
+        const size_t n = res.size();
+        unordered_map<string, int> mp;
+
+        for (int i = 0; i < n; ++i) {
+            unordered_set<string> st(res[i].begin(), res[i].end());
+            vector<string> temp(st.begin(), st.end());
+
+            for (int j = 0; j < temp.size(); ++j) {
+                mp[temp[j]]++;
+            }
+        }
+
+        string ans = "";
+        int mx = 0;
+        // one pass
+        for (auto& it : mp) {
+            if (it.second > mx ||
+                (it.second == mx && (ans.empty() || it.first < ans))) {
+                mx = it.second;
+                ans = it.first;
+            }
+        }
+        return ans;
     }
 };
