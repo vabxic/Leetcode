a2819f1eb1dba15eea8306e420de41d592c5d8f2
Vaibhav Singh <vaibhavsingh4805@gmail.com>
2026-07-06 19:46:29 +0530

    Time: 4 ms (54.15%), Space: 151.7 MB (28.93%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
         const size_t n = triplets.size();
-        vector<int>maxVal(3);
+        vector<int>maxVal = {0,0,0};
 
         for (auto t : triplets){
             if(t[0] <= target[0] && t[1] <= target[1] && t[2] <= target[2]){
@@ -10,7 +10,8 @@ public:
                 maxVal[1] = max(maxVal[1],t[1]);
                 maxVal[2] = max(maxVal[2],t[2]);
             }
+            if (maxVal == target)return true;
         }
-        return maxVal[0] == target[0] && maxVal[1] == target[1] && maxVal[2] == target[2];
+        return false;
     }
 };
