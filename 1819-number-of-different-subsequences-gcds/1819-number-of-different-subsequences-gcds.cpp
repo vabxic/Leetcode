59f39cfb1d147b69bf031e30c86285ffe6fbcc8e
Vaibhav Singh <vaibhavsingh4805@gmail.com>
2026-07-17 13:45:38 +0530

    Time: 73 ms (86.16%), Space: 75.9 MB (77.68%) - LeetHub

--- 1819-number-of-different-subsequences-gcds/1819-number-of-different-subsequences-gcds.cpp
+++ 1819-number-of-different-subsequences-gcds/1819-number-of-different-subsequences-gcds.cpp
@@ -1,3 +1,31 @@
+/*
+approach 1 - 630ms
+
+class Solution {
+public:
+    int countDifferentSubsequenceGCDs(vector<int>& nums) {
+        const size_t n = nums.size();
+
+        int count = 0;
+        int MX = *max_element(nums.begin(), nums.end());
+        unordered_set<int> seen(nums.begin(), nums.end());
+
+        for (int i = 1; i <= MX; ++i) {
+            int currGCD = 0;
+            for (int j = i; j <= MX; j += i) {
+                if (seen.count(j)) {
+                    currGCD = __gcd(currGCD, j);
+                }
+                if (currGCD == i) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+};
+*/
 class Solution {
 public:
     int countDifferentSubsequenceGCDs(vector<int>& nums) {
@@ -27,31 +55,3 @@ public:
         return ans;
     }
 };
-/*
-approach 1 - 630ms
-
-class Solution {
-public:
-    int countDifferentSubsequenceGCDs(vector<int>& nums) {
-        const size_t n = nums.size();
-
-        int count = 0;
-        int MX = *max_element(nums.begin(), nums.end());
-        unordered_set<int> seen(nums.begin(), nums.end());
-
-        for (int i = 1; i <= MX; ++i) {
-            int currGCD = 0;
-            for (int j = i; j <= MX; j += i) {
-                if (seen.count(j)) {
-                    currGCD = __gcd(currGCD, j);
-                }
-                if (currGCD == i) {
-                    count++;
-                    break;
-                }
-            }
-        }
-        return count;
-    }
-};
-*/
