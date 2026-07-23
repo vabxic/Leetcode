a4a49b1438f6b8d9ab82bedb38e28fdd62750023
Vaibhav Singh <vaibhavsingh4805@gmail.com>
2026-07-23 16:58:32 +0530

    Time: 0 ms (100%), Space: 109.4 MB (60.24%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -1,7 +1,8 @@
 /*
 approach 1
 unordered_map
-*/
+
+
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
@@ -30,7 +31,7 @@ public:
         }
         int ansLevel = 0;
         int mx = INT_MIN;
-        
+
         for (int i = 1; i < level; i++) {
             if (mp[i] > mx) {
                 mx = mp[i];
@@ -39,5 +40,43 @@ public:
         }
 
         return ansLevel;
+    }
+};
+*/
+
+//approach 2
+
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.emplace(root);
+
+        int level = 1;
+        int ans = 1;
+        int mx = INT_MIN;
+
+        while (!q.empty()) {
+            size_t sz = q.size();
+            int sum = 0;
+
+            while (sz--) {
+                TreeNode* curr = q.front();
+                q.pop();
+
+                sum += curr->val;
+                if (curr->left != NULL)
+                    q.push(curr->left);
+                if (curr->right != NULL)
+                    q.push(curr->right);
+            }
+           if (sum > mx){
+                mx = sum;
+                ans  = level;
+
+            }
+            level++;
+        }
+        return ans;
     }
 };
