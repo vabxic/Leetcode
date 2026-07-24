eeb155ab4387e375ff43fe8de637327ffbd402c5
Vaibhav Singh <vaibhavsingh4805@gmail.com>
2026-07-24 18:36:08 +0530

    Time: 4 ms (67.51%), Space: 33.4 MB (13.29%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -1,3 +1,31 @@
+/*
+approach 2
+
+class Solution {
+public:
+    TreeNode* convertToBST(ListNode* start, ListNode* end){
+        if(start == end) return nullptr;
+        ListNode* slow = start;
+        ListNode* fast = start;
+
+        while(fast!=end && fast->next!=end){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        TreeNode* head = new TreeNode(slow->val);
+        head->left = convertToBST(start, slow);
+        head->right = convertToBST(slow->next, end);
+        return head;
+    }
+
+    TreeNode* sortedListToBST(ListNode* head) {
+        if(head == nullptr) return nullptr;
+        return convertToBST(head, nullptr);
+    }
+};
+*/
+
 // approach 1
 class Solution {
 public:
@@ -11,7 +39,7 @@ public:
     }
     TreeNode* buildBST(vector<int>& nums, int st, int end) {
         if (st > end)return nullptr;
-        
+
         int mid = st + (end - st) / 2;
         TreeNode* root = new TreeNode(nums[mid]);
         root->left = buildBST(nums, st, mid - 1); // sorted vector to BST
