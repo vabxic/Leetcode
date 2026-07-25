5fee4632d26a8afc4a3db16cca176327deca27ea
Vaibhav Singh <vaibhavsingh4805@gmail.com>
2026-07-25 17:05:14 +0530

    Time: 7 ms (0.06%), Space: 21.4 MB (97.81%) - LeetHub

---
+++ 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    TreeNode* InorderSuccessor(TreeNode* root) {
+        while (root != NULL && root->left != NULL)
+            root = root->left;
+        return root;
+    }
+
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root == NULL)
+            return NULL;
+
+        if (key < root->val) {
+            root->left = deleteNode(root->left, key);
+        } else if (key > root->val) {
+            root->right = deleteNode(root->right, key);
+        } else {
+            if (root->left == NULL) {
+                return root->right;
+            } else if (root->right == NULL) {
+                return root->left;
+            } else {
+                TreeNode* IS = InorderSuccessor(root->right);
+                root->val = IS->val;
+                root->right = deleteNode(root->right, IS->val);
+            }
+        }
+
+        return root;
+    }
+
+    TreeNode* inorder(TreeNode* root, int low, int high) {
+        if (root == NULL)
+            return NULL;
+
+        root->left = inorder(root->left, low, high);
+
+        if (root->val < low || root->val > high) {
+            root = deleteNode(root, root->val);
+            return inorder(root, low, high);
+        }
+
+        root->right = inorder(root->right, low, high);
+
+        return root;
+    }
+
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        return inorder(root, low, high);
+    }
+};
