55a3414843cb9a18cd622c38d919e3147561b684
Vaibhav Singh <vaibhavsingh4805@gmail.com>
2026-07-25 17:12:55 +0530

    Time: 0 ms (100%), Space: 24.9 MB (29.09%) - LeetHub

--- 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.cpp
+++ 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.cpp
@@ -1,3 +1,6 @@
+/* approach 1
+    self made
+
 class Solution {
 public:
     TreeNode* InorderSuccessor(TreeNode* root) {
@@ -47,5 +50,27 @@ public:
 
     TreeNode* trimBST(TreeNode* root, int low, int high) {
         return inorder(root, low, high);
+    }
+};
+*/
+
+// approach 2 
+// optimal
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if (root == NULL)
+            return NULL;
+
+        if (root->val < low)
+            return trimBST(root->right, low, high);
+
+        if (root->val > high)
+            return trimBST(root->left, low, high);
+
+        root->left = trimBST(root->left, low, high);
+        root->right = trimBST(root->right, low, high);
+
+        return root;
     }
 };
