a07463faf4c1a2e5db40fd3d99bc3d00044f2b75
Vaibhav Singh <vaibhavsingh4805@gmail.com>
2026-07-28 13:28:59 +0530

    Time: 0 ms (100%), Space: 17.3 MB (17.15%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,60 @@
+// multisource graph bfs
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        size_t n = grid.size();
+        size_t m = grid[0].size();
+        int ans = 0;
+
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
+        queue<pair<pair<int, int>, int>> q; // (i,j),time;
+
+        // push all sources
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == 2) {
+                    q.push({{i, j}, 0});
+                    vis[i][j] = true;
+                }
+            }
+        }
+        // bfs
+        while (!q.empty()) {
+            int i = q.front().first.first;
+            int j = q.front().first.second;
+            int time = q.front().second;
+            q.pop();
+
+            ans = max(ans, time);
+
+            if (i - 1 >= 0 && !vis[i - 1][j] && grid[i - 1][j] == 1) { // top
+                q.push({{i - 1, j}, time + 1});
+                vis[i - 1][j] = true;
+            }
+
+            if (i + 1 < n && !vis[i + 1][j] && grid[i + 1][j] == 1) { // bottom
+                q.push({{i + 1, j}, time + 1});
+                vis[i + 1][j] = true;
+            }
+
+            if (j + 1 < m && !vis[i][j + 1] && grid[i][j + 1] == 1) { // right
+                q.push({{i, j + 1}, time + 1});
+                vis[i][j + 1] = true;
+            }
+
+            if (j - 1 >= 0 && !vis[i][j - 1] && grid[i][j - 1] == 1) { // left
+                q.push({{i, j - 1}, time + 1});
+                vis[i][j - 1] = true;
+            }
+        }
+        // fresh orange
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == 1 && !vis[i][j]) {
+                    return -1;
+                }
+            }
+        }
+        return ans;
+    }
+};
