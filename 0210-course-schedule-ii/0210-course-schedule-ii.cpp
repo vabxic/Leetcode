63622c5e888db03ff6ba0452b423d941c66fd981
Vaibhav Singh <vaibhavsingh4805@gmail.com>
2026-07-28 18:01:37 +0530

    Time: 66 ms (5.1%), Space: 17.4 MB (98.61%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+    bool isCycleDirDFS(int curr, vector<bool>& vis, vector<bool>& recPath,
+                       vector<vector<int>>& edges) {
+        vis[curr] = true;
+        recPath[curr] = true;
+
+        for (int i = 0; i < edges.size(); i++) {
+            int v = edges[i][0];
+            int u = edges[i][1];
+
+            if (u == curr) {
+                if (!vis[v]) {
+                    if (isCycleDirDFS(v, vis, recPath, edges))
+                        return true;
+                } else if (recPath[v]) {
+                    return true;
+                }
+            }
+        }
+
+        recPath[curr] = false;
+        return false;
+    }
+
+    void dfs(int curr, vector<bool>& vis, stack<int>& s,
+             vector<vector<int>>& edges) {
+
+        vis[curr] = true;
+
+        for (int i = 0; i < edges.size(); i++) {
+            int v = edges[i][0];
+            int u = edges[i][1];
+
+            if (u == curr) {
+                if (!vis[v])
+                    dfs(v, vis, s, edges);
+            }
+        }
+
+        s.push(curr);
+    }
+
+    vector<int> findOrder(int n, vector<vector<int>>& edges) {
+
+        vector<bool> vis(n, false);
+        vector<bool> recPath(n, false);
+
+        // Detect cycle
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                if (isCycleDirDFS(i, vis, recPath, edges))
+                    return {};
+            }
+        }
+
+        // Reset visited for Topological Sort
+        fill(vis.begin(), vis.end(), false);
+
+        stack<int> s;
+        for (int i = 0; i < n; i++) {
+            if (!vis[i])
+                dfs(i, vis, s, edges);
+        }
+
+        vector<int> ans;
+
+        while (!s.empty()) {
+            ans.push_back(s.top());
+            s.pop();
+        }
+        return ans;
+    }
+};
