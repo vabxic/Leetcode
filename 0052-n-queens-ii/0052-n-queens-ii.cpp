560158ccc3fccbc8f8a9072584e4b317b1cd35c5
Vaibhav Singh <vaibhavsingh4805@gmail.com>
2026-08-04 15:08:24 +0530

    Time: 4 ms (20.39%), Space: 8.3 MB (54.49%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int ans;
+    vector<string> board;
+    int n;
+
+    bool safe(int row, int col) {
+
+        // same col , horizontal
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q')
+                return false;
+        }
+
+        // upper-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        // upper-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        return true;
+    }
+
+    void backTrack(int row) {
+        if (row == n) {
+            ans++;
+            return;
+        }
+
+        for (int col = 0; col < n; ++col) {
+            if (!safe(row, col)) {
+                continue;
+            }
+
+            // choose
+            board[row][col] = 'Q';
+
+            // explore
+            backTrack(row + 1);
+
+            // undo
+            board[row][col] = '.';
+        }
+    }
+
+    int totalNQueens(int N) {
+        n = N;
+        board = vector<string>(n, (string(n, '.')));
+
+        backTrack(0);
+        return ans;
+    }
+};
