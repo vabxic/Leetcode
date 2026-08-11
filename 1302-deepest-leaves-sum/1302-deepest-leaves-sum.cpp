6debf4c6af22141a9962f0c883786b6de375e078
Vaibhav Singh <vaibhavsingh4805@gmail.com>
2026-08-11 20:52:53 +0530

    Time: 0 ms (100%), Space: 62.4 MB (65.04%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -1,6 +1,6 @@
+
 /*
 sc - o(n)
-*/
 class Solution {
 public:
     vector<int> deep;
@@ -33,5 +33,41 @@ public:
     int deepestLeavesSum(TreeNode* root) {
         bfs(root);
         return accumulate(deep.begin(), deep.end(), 0);
+    }
+};
+*/
+
+// sc - o(1);
+class Solution {
+public:
+    int sum = 0;
+    void bfs(TreeNode* root) {
+        if (root == NULL)
+            return;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            size_t sz = q.size();
+
+            sum = 0;
+            while (sz--) {
+                TreeNode* curr = q.front();
+                q.pop();
+
+                sum += curr->val;
+                if (curr->left != NULL) {
+                    q.push(curr->left);
+                }
+                if (curr->right != NULL) {
+                    q.push(curr->right);
+                }
+            }
+        }
+    }
+    int deepestLeavesSum(TreeNode* root) {
+        bfs(root);
+        return sum;
     }
 };
