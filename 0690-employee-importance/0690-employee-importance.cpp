0d50f5178065672639073d09dc7f3c8c788b2e5c
Vaibhav Singh <vaibhavsingh4805@gmail.com>
2026-08-18 21:02:28 +0530

    Time: 23 ms (73.01%), Space: 31.4 MB (8.52%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int getImportance(vector<Employee*> employees, int id) {
+        
+        int result = 0;
+        for(auto& emp : employees){
+            if(emp-> id == id){
+                result = emp->importance;;
+                if(!emp->subordinates.empty()){
+                    for(auto& s : emp->subordinates){
+                        result += getImportance(employees, s);
+                    }
+                }
+                return result;
+            }
+        }
+        return result;
+    }
+};
